bd444211e32522cfd4e88a7fcd1609bbac44f931
prog4food <3475861+prog4food@users.noreply.github.com>
2023-03-02 01:59:17 +0300

    + новый логотип и адрес

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-PKG=ottplay-foss.samsung_orsay_720p_v001
+VER=002
+PKG=ottplay-foss.samsung_orsay_720p_v$VER
 rm "./${PKG}.zip" > /dev/null 2>&1
 cd "./app_DEFHJ/"
 zip -r "../${PKG}.zip" *
 cd ..
 
-PKG=ottplay-foss.samsung_h_720p_v001
+PKG=ottplay-foss.samsung_h_720p_v$VER
 rm "./${PKG}.zip" > /dev/null 2>&1
 cd "./app_H/"
 zip -r "../${PKG}.zip" *
