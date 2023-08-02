85a25e7e73d03572033077b18af34ddcda1beccd
prog4food <3475861+prog4food@users.noreply.github.com>
2023-08-02 18:52:14 +0300

    + автоматический установщик в память ТВ + автоматический распаковщик на флешку + иконки теперь в отдельном каталоге + новый скрипт деплоя + gitignore

---
+++ app_installer/js/main.js
@@ -0,0 +1,77 @@
+var Log_area, Log_string = '',
+  Name = 'ottplay-foss.orsay',
+  zipDir, oFile, widgetAPI, tvKey;
+
+function _logFormat(s) {
+  return s.replace(/<span_([^>]+)>/g, '<span class=\'\$1\'>');
+}
+
+try {
+  widgetAPI = new Common.API.Widget();
+  tvKey = new Common.API.TVKeyValue();
+} catch (e) {
+  Log_string += _logFormat('<span_red>Cannot use device API</span><br /><pre>') + e + '</pre><br />';
+}
+
+function OnLoad() {
+  Log_area = document.getElementById('Log_area');
+  document.getElementById('anchor').focus();
+
+  if (typeof widgetAPI === 'undefined') {
+    Log_area.innerHTML = Log_string;
+  }
+
+  widgetAPI.sendReadyEvent();
+  Log('<h2>Start!</h2>');
+  setTimeout('Install()', 2000);
+}
+
+function Install() {
+  oFile = document.getElementById('pluginObjectFile');
+  var oStorage = document.getElementById('pluginStorage'),
+    c = oStorage.GetUSBListSize(), n, id;
+  Log('Found <span_gray>' + c + '</span> USB Device');
+  for (var i = 0; i < c; i++) {
+    id = Number(oStorage.GetUSBDeviceID(i));
+    Log('Vendor: <span_gray>' + oStorage.GetUSBVendorName(id) + '</span>, Model: <span_gray>' + oStorage.GetUSBModelName(id) + '</span><br />');
+    n = oStorage.GetUSBPartitionNum(id);
+    for (var j = 0; j < n; j++) {
+      zipDir = '/dtv/usb/' + oStorage.GetUSBMountPath(id, j) + '/' + Name + '.installer/data/';
+      if (oFile.IsExistedPath(zipDir)) {
+        Log('USBMountPath: <span_gray>' + zipDir + '</span><br />');
+        return setTimeout('F1()', 1000);
+      }
+    }
+  }
+
+  Log('The installation files not found on USB.<br />Press any key for EXIT.');
+}
+
+function F1() {
+  if (oFile.IsExistedPath(zipDir + Name + '.zip')) {
+    Log('Installation ! &nbsp; &nbsp; Please Wait 20-30 sec . . . .');
+    return setTimeout('F2()', 100);
+  }
+
+  Log('The installation files not found on USB.<br />Press any key for EXIT.');
+}
+
+function F2() {
+  var r = oFile.Unzip(zipDir + Name + '.zip', '/mtd_down/widgets/user/');
+  Log('Installation <span_gray>' + Name + '.zip</span>: <span_' + (r == 1 ? 'green>&#10004; OK!' : 'red>&#10006; Error! ' + r.toString()) + '</span><br />');
+  Log('Press any key for EXIT !');
+}
+
+function OnUnload() {}
+
+function KeyDown() {
+  if (event) {
+    event.preventDefault();
+  }
+  widgetAPI.sendExitEvent();
+}
+
+function Log(s) {
+  Log_string += _logFormat(s) + '<br />';
+  widgetAPI.putInnerHTML(Log_area, Log_string);
+}
--- build.sh
+++ build.sh
@@ -1,13 +1,59 @@
 #!/bin/sh
-VER=002
-PKG=ottplay-foss.samsung_orsay_720p_v$VER
-rm "./${PKG}.zip" > /dev/null 2>&1
-cd "./app_DEFHJ/"
-zip -r "../${PKG}.zip" *
-cd ..
+APP_ID="ottplay-foss.orsay"
+APP_ICONS="ott85x70.png ott95x78.png ott106x87.png ott115x95.png"
 
-PKG=ottplay-foss.samsung_h_720p_v$VER
-rm "./${PKG}.zip" > /dev/null 2>&1
-cd "./app_H/"
-zip -r "../${PKG}.zip" *
-cd ..
+APP_VER=2
+INSTALLER_VER=1
+
+make_app() {
+  echo "Packing app for: $1"
+  pkg_zip="usb-app_${APP_ID}_${1}_720p.v${APP_VER}.zip"
+  pkg_dir="${APP_ID}.app"
+
+  # Clean
+  [ -d "$pkg_dir" ] && rm -rf $pkg_dir
+
+  # Prepare
+  mkdir $pkg_dir
+  cp -a -t $pkg_dir _app_base/* app_$1/*
+
+  # Pack
+  zip -r dist/$pkg_zip $pkg_dir
+  cat unzipsfx6_overwrite_prog4food.stub dist/$pkg_zip > dist/${pkg_zip%.*}.exe
+
+  # Clean
+  rm -rf $pkg_dir
+
+  # Build installer
+  make_installer $1 $pkg_zip
+}
+
+make_installer() {
+  echo "Packing app installer for: $pkg_zip"
+  pkg_zip="tv-installer_${APP_ID}_${1}_720p.v${INSTALLER_VER}_${APP_VER}.zip"
+  pkg_dir="${APP_ID}.installer"
+
+  # Clean
+  [ -d "$pkg_dir" ] && rm -rf $pkg_dir
+
+  # Prepare
+  mkdir $pkg_dir
+  cp -a -t $pkg_dir app_installer/*
+  for f in $APP_ICONS; do cp -a -t $pkg_dir/icon/ _app_base/$f; done
+  cp -a dist/$2 $pkg_dir/data/ottplay-foss.orsay.zip
+
+  # Pack
+  zip -r dist/$pkg_zip $pkg_dir
+  cat unzipsfx6_overwrite_prog4food.stub dist/$pkg_zip > dist/${pkg_zip%.*}.exe
+
+  # Clean
+  rm -rf $pkg_dir
+}
+
+[ -d "dist" ] && rm -rf dist
+mkdir dist
+
+make_app "DEFHJ"
+make_app "H"
+
+echo "All done!"
